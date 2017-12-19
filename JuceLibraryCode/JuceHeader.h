2fcae8841c5d432e2d690c6cd540f1ea22833c03
Akiyuki Okayasu <akiyuki@tkrworks.net>
2017-12-19 17:09:32 +0900

    v0.3.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "ImpulseResponseMeasurement";
-    const char* const  versionString  = "0.2.0";
-    const int          versionNumber  = 0x200;
+    const char* const  versionString  = "0.3.0";
+    const int          versionNumber  = 0x300;
 }
 #endif
